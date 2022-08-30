63d728ded12dbc9871e1c518cf29c9d61c463eae
Olarewaju34 <mohmzyo@gmail.com>
2022-08-30 13:41:24 +0100

    Code Challenge

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+         Console.WriteLine("Enter a string of numbers: ");
+        string Numbers = Console.ReadLine().Trim();
+        string result =LeftRightNumbers(Numbers);  
+        Console.WriteLine(result);
+
+       Console.ReadKey();       
+    }
+     static string LeftRightNumbers(string Letters)
+    {
+        int firstsum = 0;
+        int secondsum = 0;
+
+        string RightNumbers = Letters.Substring(0, Letters.Length / 2);
+        string LeftNumbers = Letters.Substring(Letters.Length / 2);
+
+        for (int i = 0; i < RightNumbers.Length; i++)
+        {
+            firstsum += RightNumbers[i] - '0';
+            secondsum += LeftNumbers[i] - '0';
+        }
+       return firstsum == secondsum? "yes":"no";
+    }
+
+
+
+}
